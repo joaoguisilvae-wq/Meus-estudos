1dedf41d87fdab338c9154d42c55d0459e845f62
joaoguisilvae-wq <joaogui.silvae@gmail.com>
2025-11-07 09:33:08 -0300

    esqueci disso

--- MeuEstudoJS/8 - Programação assincrona com js/js/scripts.js
+++ MeuEstudoJS/8 - Programação assincrona com js/js/scripts.js
@@ -10,8 +10,8 @@ console.log("Ainda não executou2");
 // 2 - setInterval
 console.log("ainda não começou");
 
-setInterval(function () {
-  console.log("intervalo assincrono");
-}, 3000);
+// setInterval(function () {
+//   console.log("intervalo assincrono");
+// }, 3000);
 
 console.log("ainda não começou2");
