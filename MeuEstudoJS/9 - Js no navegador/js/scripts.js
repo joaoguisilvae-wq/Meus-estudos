a852f9fd0b0cc4be75efbd73ce726cf5b6ee1bad
joaoguisilvae-wq <joaogui.silvae@gmail.com>
2025-11-09 20:43:13 -0300

    agr ss td certo

--- MeuEstudoJS/9 - Js no navegador/js/scripts.js
+++ MeuEstudoJS/9 - Js no navegador/js/scripts.js
@@ -1,1 +1,8 @@
-onclick(console.log("aaa"));
+// 1 - movendo-se pelo DOM
+console.log(document.body);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[3].textContent);
