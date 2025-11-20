8d8d607113ac3f11ad72a238fcee2b966aeb4c3a
joaoguisilvae-wq <joaogui.silvae@gmail.com>
2025-11-20 17:10:33 -0300

    a

--- MeuEstudoJS/12 - Axios/js/scripts.js
+++ MeuEstudoJS/12 - Axios/js/scripts.js
@@ -5,13 +5,36 @@ console.log(axios);
 const getData = async () => {
   try {
     const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/user"
+      "https://jsonplaceholder.typicode.com/users"
     );
 
     console.log(response);
+
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 };
 
 getData();
+
+// 3 - Inprimindo dados na tela
+const container = document.querySelector("#user-container");
+
+const printData = async () => {
+  const data = await getData();
+
+  data.forEach((user) => {
+    const div = document.createElement("div");
+
+    const nameElement = document.createElement("h2")
+
+    nameElement.textContent = user.name
+
+    div.appendChild(nameElement)
+
+    contain
+  });
+};
+printData();
+
